1fed9056f52a06205c7894c387dc874182b7fe76
limjisooooooo <trefrasd@gmail.com>
2022-03-29 22:57:40 +0900

    ...

--- src/CheckImg.tsx
+++ src/CheckImg.tsx
@@ -16,19 +16,26 @@ const CheckImg = () => {
     React.useEffect(() => {
         ctx.current = canvas.current!.getContext("2d");
         canvas.current!.width = parseInt(getComputedStyle(canvas.current!).getPropertyValue('width'));
-        canvas.current!.height = parseInt(getComputedStyle(canvas.current!).getPropertyValue('height'));
+        const image = new Image();
+        image.src="/img/notebook.png";
+        image.onload = () => {
+            canvas.current!.style.height=((canvas.current!.width / image.width) * image.height).toString();
+            canvas.current!.height = parseInt(getComputedStyle(canvas.current!).getPropertyValue('height'));
+        }
+        //canvas.current!.style.height="500px";        
         ctx.current!.strokeStyle="#000";
         ctx.current!.lineWidth= 2.5;                  
     },[])        
-    console.log("Ren")
+
     return (
         <>            
             <div/>
-            <canvas ref={canvas} style={{border: "0.1em solid #000", width:"100%", margin:"5% 0", backgroundImage:`url("/img/notebook.png")`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}} 
-            onMouseDown={() => {painting.current = true}} 
-            onMouseMove={(e) => {onMouseMove(e)}} 
-            onMouseUp={() => {painting.current = false}} 
-            onMouseLeave={() => {painting.current = false}}/>                            
+            {/*<canvas ref={canvas} style={{border: "0.1em solid #000", width:"100%", margin:"5% 0", backgroundImage:`url("/img/notebook.png")`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}} */}
+            <canvas ref={canvas} style={{border: "0.1em solid #000", width:"100%", margin:"5% 0"}} 
+                onMouseDown={() => {painting.current = true}} 
+                onMouseMove={(e) => {onMouseMove(e)}} 
+                onMouseUp={() => {painting.current = false}} 
+                onMouseLeave={() => {painting.current = false}}/>                            
         </>
     )
 }
